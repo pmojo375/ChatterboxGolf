c938451db7e7b4d2193aeb2d49925c624512988f
pmojo375 <pmojo375@gmail.com>
2019-06-16 21:03:19 -0400

    added viewport scaling meta tag to all pages

--- weeks/5.php
+++ weeks/5.php
@@ -6,6 +6,7 @@
     <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate"/>
     <meta http-equiv="Pragma" content="no-cache"/>
     <meta http-equiv="Expires" content="0"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Chatterbox Golf League - Week 5</title>
     <meta name="description" content="Week 5 Stats and Standings">
     <link rel="stylesheet" href="../main.css?rnd=23">
